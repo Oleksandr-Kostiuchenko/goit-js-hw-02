2c3abb2a4df8cbcdad6b863459f64c1742908563
Oleksandr Kostiuchenko <aleksandrkostucenko11@gmail.com>
2024-11-05 20:43:01 +0100

    finish

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,17 @@
-const content = undefined;
-const padding = undefined;
-const border = undefined;
-
-function getElementWidth(content, padding, border) {
-    const totalWidth = Number.parseFloat(content) + (Number.parseFloat(padding) * 2) + (Number.parseFloat(border) * 2);
-
-    return totalWidth;
+function checkForSpam(message){
+    const lowerMessage = message.toLowerCase();
+    
+    if(lowerMessage.includes("spam") || lowerMessage.includes("sale")){
+        return true;
+    } else{
+        return false;
+    }
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(checkForSpam("Latest technology news"));
+console.log(checkForSpam("JavaScript weekly newsletter"));
+console.log(checkForSpam("Get best sale offers now!"));
+console.log(checkForSpam("Amazing SalE, only tonight!")); 
+console.log(checkForSpam("Trust me, this is not a spam message"));
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
+console.log(checkForSpam("[SPAM] How to earn fast money?"));
